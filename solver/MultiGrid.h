01bf6a17dc3472ba7cfdd9ea07bcc5954fdfdef5
jackywave <zj15071795262@163.com>
2023-12-07 01:40:37 +0800

    add solver

---
+++ solver/MultiGrid.h
@@ -0,0 +1,14 @@
+//
+// Created by zhangjie on 2023/12/7.
+//
+
+#ifndef SIMPLEXOLDCLION_MULTIGRID_H
+#define SIMPLEXOLDCLION_MULTIGRID_H
+
+
+class MultiGrid {
+
+};
+
+
+#endif //SIMPLEXOLDCLION_MULTIGRID_H
